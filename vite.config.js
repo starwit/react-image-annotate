efccddfc5ce930897ec38cb61c7193259bafdbb7
Anett Hübner <anett_huebner@gmx.de>
2025-01-20 11:09:24 +0100

    update vite, removed polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import markdownRawPlugin from 'vite-raw-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    nodePolyfills(),
     markdownRawPlugin({
       fileRegex: /\.md$/
     }),
