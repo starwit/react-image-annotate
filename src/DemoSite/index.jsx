bf2bb78fd440f1dc54a800450f327761003f61a1
flonix8 <flstanek@googlemail.com>
2026-07-01 12:26:12 +0200

    Inline DemoSite examples (because there is only one left)

--- src/DemoSite/index.jsx
+++ src/DemoSite/index.jsx
@@ -1,9 +1,25 @@
 import {useRef, useState} from "react"
-import {examples} from "./Examples.jsx"
+import {produce} from "immer"
 import Annotator from "../Annotator"
 
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case "SELECT_CLASSIFICATION": {
+      switch (action.cls) {
+        case "Line-Crossing": {
+          return produce(state, s => {s.selectedTool = "create-line"})
+        }
+        case "Area-Occupancy": {
+          return produce(state, s => {s.selectedTool = "create-polygon"})
+        }
+      }
+    }
+  }
+
+  return state;
+};
+
 export default () => {
-  const annotatorProps = examples["Constrained Tools"]();
   const [movementLocked, setMovementLocked] = useState(false)
   const annotatorRef = useRef(null)
 
@@ -27,7 +43,14 @@ export default () => {
       </div>
       <div style={{flex: 1, minHeight: 0, position: "relative"}}>
         <Annotator
-          {...(annotatorProps)}
+          regionClsList={["Line-Crossing", "Area-Occupancy"]}
+          regionColorList={["#00da86", "#1e87e9"]}
+          image={{
+            src: "https://images.unsplash.com/photo-1567563549378-81212b9631e4?q=80&w=1548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            name: "intersection",
+          }}
+          userReducer={userReducer}
+          enabledRegionProps={["name", "line-direction"]}
           ref={annotatorRef}
           movementLocked={movementLocked}
         />
